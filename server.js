d1ea3a06516e6d51960a4cc87d39a95649d80cf8
Ashley Radford <aradford7@outlook.com>
2019-04-15 12:52:28 -0700

    add crud objects

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+/////Constants///////////////////////
 const express = require('express');
 const app = express();
 const port = 3000;
+//db constant
+const GOT = require('./models/GotHouses');
 const bodyParser = require ('body-parser');
 const methodOverride = require ('method-override');
+///////////////////////////////////////////////////
+//////////MiddleWare/////////////////////////////
+
+/////////////////////////////////////////////////
+//index route show GOT connection
+app.get('/got',(req,res)=>{
+    res.send(GOT);
+});
 ///////////// set listener//////////////
 app.listen(port,()=>{
     console.log("app listening on PartnerLab port:",port);
