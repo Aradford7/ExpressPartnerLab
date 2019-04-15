aabddfd1f5a40565ed722a93877ddaab5633e1bd
Ashley Radford <aradford7@outlook.com>
2019-04-15 13:09:49 -0700

    add index route that display all objects

--- server.js
+++ server.js
@@ -11,8 +11,18 @@ const methodOverride = require ('method-override');
 
 /////////////////////////////////////////////////
 //index route show GOT connection
-app.get('/got',(req,res)=>{
+app.get('/got/index',(req,res)=>{
     res.send(GOT);
+});
+///create the index route
+app.get('/got',(req,res)=>{
+    res.render('index.ejs',{GotHouses:GOT})
+});
+//create the show route
+app.get('/got/:id', (req,res) => {
+    res.render('show.ejs',{
+        GotHouses:GOT[req.params.id]
+    });
 });
 ///////////// set listener//////////////
 app.listen(port,()=>{
