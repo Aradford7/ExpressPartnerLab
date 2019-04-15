293bb079af805676e4ef60d3895ffedaef7f2a0f
Ashley Radford <aradford7@outlook.com>
2019-04-15 11:55:59 -0700

    add got array

---
+++ models/gothouses.js
@@ -0,0 +1,84 @@
+const GotHouses = [
+    {
+      name: "Daenerys", 
+      house: "Targaryen",
+      status: "Alive",
+      img: "http://tinyurl.com/yyz5mz6b"
+    },
+    {
+      name: "Viserys", 
+      house: "Targaryen",
+      status: "Dead",
+      img: "http://tinyurl.com/y3bmyc4s"
+    },
+    {
+      name: "Aegon (Jon Snow)", 
+      house: "Targaryen",
+      status: "Alive",
+      img: "http://tinyurl.com/yxs82xet"
+      },
+    {
+      name: "Sansa", 
+      house: "Stark",
+      status: "Alive",
+      img: "http://tinyurl.com/yyk8m4ce"
+    },
+    {
+      name: "Arya", 
+      house: "Stark",
+      status: "Alive",
+      img: "https://s1.r29static.com//bin/entry/e5a/720x864,85/1992581/image.webp"
+    },
+    {
+      name: "Ned", 
+      house: "Stark",
+      status: "Dead",
+      img: "http://tinyurl.com/yyscj3uk"
+    },
+    {
+      name: "Cersei", 
+      house: "Lannister",
+      status: "Alive",
+      img: "http://tinyurl.com/y37femos",
+    },
+    {
+      name: "Tyrion", 
+      house: "Lannister",
+      status: "Alive",
+      img: "http://tinyurl.com/y5nrecwx",
+    },
+    {
+      name: "Joffery", 
+      house: "Lannister",
+      status: "Dead",
+      img: "http://tinyurl.com/yxw2ytqn",
+    },
+    {
+      name: "Sam", 
+      house: "Tarly",
+      status: "Alive",
+      img: "http://tinyurl.com/yxr2yhcm",
+    },
+    {
+      name: "Milsandre", 
+      house: "Unknown",
+      status: "Unknown",
+      img: "http://tinyurl.com/y2lt4waf",
+    },
+    {
+      name: "Hound", 
+      house: "Unknown",
+      status: "Unknown",
+      img: "http://tinyurl.com/y4qhor3c",
+    },
+    {
+      name: "Khal Drogo", 
+      house: "Dothraki",
+      status: "Dead",
+      img: "http://tinyurl.com/y2mgg2z5",
+    },
+
+  ];
+  ///set up database so this file can be exported to server.js///
+  module.exports = GotHouses;
+  //chai.request(server);
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-
-
+const bodyParser = require ('body-parser');
+const methodOverride = require ('method-override');
 ///////////// set listener//////////////
 app.listen(port,()=>{
     console.log("app listening on PartnerLab port:",port);
